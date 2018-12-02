d25b3f1effcdaf07af24d48e641104d3ae200910
skenoy <skenoy@gmail.com>
2018-12-02 11:44:17 +0800

    完成前端模版设计，所有页面已调试完毕

--- app/admin/views.py
+++ app/admin/views.py
@@ -3,9 +3,11 @@
 from . import admin
 from flask import render_template, redirect, url_for
 
+
 @admin.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('admin/index.html')
+
 
 @admin.route('/login/')
 def login():
@@ -14,4 +16,39 @@ def login():
 
 @admin.route('/logout/')
 def logout():
-    return redirect(url_for('admin.login'))
+    return redirect(url_for('login'))
+
+
+@admin.route('/videolist/')
+def videolist():
+    return render_template('admin/videolist.html')
+
+
+@admin.route('/playvideo/')
+def playvideo():
+    return render_template('admin/playvideo.html')
+
+
+@admin.route('/biotoolslist/')
+def biotoolslist():
+    return render_template('admin/biotoolslist.html')
+
+
+@admin.route('/infotoolslist/')
+def infotoolslist():
+    return render_template('admin/infotoolslist.html')
+
+
+@admin.route('/runtool/')
+def runtool():
+    return render_template('admin/runtool.html')
+
+
+@admin.route('/profile/')
+def profile():
+    return render_template('admin/profile.html')
+
+
+@admin.route('/loginlog/')
+def loginlog():
+    return render_template('admin/loginlog.html')
