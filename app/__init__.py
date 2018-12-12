63635b5e89f72735b2daf61ac08faa2da4b6d294
zhengpanone <zhengpanone@hotmail.com>
2018-12-12 16:23:04 +0800

    添加403页面，并注册到app对象，修改403，404页面部分样式

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,10 @@ def create_app():
     def page_not_found(error):
         return render_template('404.html'), 404
 
+    @app.errorhandler(403)
+    def page_forbidden(error):
+        return render_template('403.html'), 403
+
     with app.app_context():
         db.create_all()
     return app
