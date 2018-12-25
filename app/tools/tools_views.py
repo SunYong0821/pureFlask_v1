292480b2da8c4086139c30d0c715c9ffcef1cd8a
skenoy <skenoy@gmail.com>
2018-12-25 14:53:45 +0800

    fbug

--- app/tools/tools_views.py
+++ app/tools/tools_views.py
@@ -89,7 +89,7 @@ def pooling():
         with open(f"{taskdir}/run.log", "w") as optfile:
             optfile.write(
                 f"Options: {form.lane.data} {form.vol.data} {form.sizes.data}\n")
-        script = f"""python ./app/static/program/pooling/libraryPooling.py {inputfile} {form.lane.data} {form.vol.data} {form.sizes.data} 2>>{taskdir}/run.log"""
+        script = f"python ./app/static/program/pooling/libraryPooling.py {inputfile} {form.lane.data} {form.vol.data} {form.sizes.data} 2>>{taskdir}/run.log"
         app = current_app._get_current_object()
         crun = threading.Thread(target=runtools, args=(app, script, uuid))
         crun.start()
@@ -127,9 +127,9 @@ def deg_filter():
             optfile.write(
                 f"Options: {form.fc.data} {form.fccol.data} {form.pq.data} {form.yuzhi.data} {form.pqcol.data} {form.outpre.data}\n")
         if form.pq.data == "1":
-            script = f"""perl ./app/static/program/deg_filter/Select_DiffexpGene.pl -i {inputfile} -fc {form.fc.data} -fccolumn {form.fccol.data} -pvalue {form.yuzhi.data} -pcolumn {form.pqcol.data} -head -prefix {form.outpre.data} 2>>{taskdir}/run.log"""
+            script = f"perl ./app/static/program/deg_filter/Select_DiffexpGene.pl -i {inputfile} -fc {form.fc.data} -fccolumn {form.fccol.data} -pvalue {form.yuzhi.data} -pcolumn {form.pqcol.data} -head -prefix {form.outpre.data} 2>>{taskdir}/run.log"
         else:
-            script = f"""perl ./app/static/program/deg_filter/Select_DiffexpGene.pl -i {inputfile} -fc {form.fc.data} -fccolumn {form.fccol.data} -fdr {form.yuzhi.data} -fdrcolumn {form.pqcol.data} -head -prefix {form.outpre.data} 2>>{taskdir}/run.log"""
+            script = f"perl ./app/static/program/deg_filter/Select_DiffexpGene.pl -i {inputfile} -fc {form.fc.data} -fccolumn {form.fccol.data} -fdr {form.yuzhi.data} -fdrcolumn {form.pqcol.data} -head -prefix {form.outpre.data} 2>>{taskdir}/run.log"
         app = current_app._get_current_object()
         crun = threading.Thread(target=runtools, args=(app, script, uuid))
         crun.start()
@@ -148,7 +148,7 @@ def volcano():
         with open(f"{taskdir}/run.log", "w") as optfile:
             optfile.write(
                 f"Options: {form.fc.data} {form.fccol.data} {form.pq.data} {form.pqcol.data} {form.outpre.data}\n")
-        script = f"""perl ./app/static/program/volcano/Volcano_plot.pl -i {inputfile} -f {form.fc.data} -log2col {form.fccol.data} -pvalue {form.pq.data} -pCol {form.pqcol.data} -prefix {form.outpre.data} 2>>{taskdir}/run.log"""
+        script = f"perl ./app/static/program/volcano/Volcano_plot.pl -i {inputfile} -f {form.fc.data} -log2col {form.fccol.data} -pvalue {form.pq.data} -pCol {form.pqcol.data} -prefix {form.outpre.data} 2>>{taskdir}/run.log"
         app = current_app._get_current_object()
         crun = threading.Thread(target=runtools, args=(app, script, uuid))
         crun.start()
@@ -167,7 +167,7 @@ def ma_plot():
         with open(f"{taskdir}/run.log", "w") as optfile:
             optfile.write(
                 f"Options: {form.fc.data} {form.fccol.data} {form.pq.data} {form.pqcol.data} {form.exp1.data} {form.exp2.data} {form.outpre.data}\n")
-        script = f"""perl ./app/static/program/ma_plot/MA_plot.pl -i {inputfile} -log2col {form.fccol.data} -exp1col {form.exp1.data} -exp2col {form.exp2.data} -pvalue {form.pq.data} -pCol {form.pqcol.data} -prefix {form.outpre.data} -f {form.fc.data} 2>>{taskdir}/run.log"""
+        script = f"perl ./app/static/program/ma_plot/MA_plot.pl -i {inputfile} -log2col {form.fccol.data} -exp1col {form.exp1.data} -exp2col {form.exp2.data} -pvalue {form.pq.data} -pCol {form.pqcol.data} -prefix {form.outpre.data} -f {form.fc.data} 2>>{taskdir}/run.log"
         app = current_app._get_current_object()
         crun = threading.Thread(target=runtools, args=(app, script, uuid))
         crun.start()
@@ -215,7 +215,7 @@ def ezcollinear():
         with open(f"{taskdir}/run.log", "w") as optfile:
             optfile.write(
                 f"Options: {form.fai1.data} {form.fai2.data} {form.links.data} {form.name.data} {form.opacity.data} {form.outpre.data}\n")
-        script = f"""perl ./app/static/program/ezcollinear/collinearity.pl {form.fai1.data},{form.fai2.data} {form.name.data} {form.links.data} {form.outpre.data} {form.opacity.data} 2>>{taskdir}/run.log"""
+        script = f"perl ./app/static/program/ezcollinear/collinearity.pl {form.fai1.data},{form.fai2.data} {form.name.data} {form.links.data} {form.outpre.data} {form.opacity.data} 2>>{taskdir}/run.log"
         app = current_app._get_current_object()
         crun = threading.Thread(target=runtools, args=(app, script, uuid))
         crun.start()
