2d98dabe037830eaf7990ecf703c53808be236c3
skenoy <skenoy@gmail.com>
2019-01-02 17:00:16 +0800

    热图bug测试5

--- app/static/program/pheatmap/heatmap.r
+++ app/static/program/pheatmap/heatmap.r
@@ -4,6 +4,6 @@ data = read.table(args[10], header = TRUE, sep = "\t", check.name = FALSE)
 rownames(data) = data[,1]
 data = data[,2:length(data[1,])]
 data = as.matrix(data)
-pdf(file = args[9], width = args[7], height = args[8])
-pheatmap(data, scale = args[1], cluster_rows = args[2], cluster_cols = args[3], show_rownames = args[4] show_colnames=args[5] display_numbers=args[6])
+pdf(file = paste("out/", args[9], sep=""), width = args[7], height = args[8])
+pheatmap(data, scale = args[1], cluster_rows = args[2], cluster_cols = args[3], show_rownames = args[4], show_colnames=args[5], display_numbers=args[6])
 dev.off()
