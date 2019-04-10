519eae6b23e50a4fb664781d17f04e6f0566a08c
zhengpanone <zhengpanone@hotmail.com>
2019-04-10 16:50:13 +0800

    modified worh.sh

--- app/static/program/spearman/work.sh
+++ app/static/program/spearman/work.sh
@@ -1,1 +1,1 @@
-perl /home/test/04.spearman/spearman_plot.pl -i /home/test/04.spearman/genus_diff_top30.txt -outdir /home/test/04.spearman
+perl /opt/magewb_test/magweb/app/static/program/spearman/spearman_plot.pl -i /opt/magewb_test/magweb/app/static/program/spearman/genus_diff_top30.txt
