55a0b369038e3a46af55dcff21bf2a0e28a38513
weidr <yuderuii@gmail.com>
2019-04-19 14:21:06 +0800

    修改vcf2phylip程序bug

--- app/static/program/vcf2phylip/vcf2phylip.py
+++ app/static/program/vcf2phylip/vcf2phylip.py
@@ -3,6 +3,7 @@ from shutil import make_archive
 
 path = os.path.dirname(os.path.abspath(sys.argv[0]))
 inputdir = os.path.dirname(os.path.abspath(sys.argv[1]))
+inputname = os.path.basename(os.path.abspath(sys.argv[1]))
 
 outdir = inputdir + "/out"
 if os.path.exists(outdir):
@@ -11,9 +12,7 @@ os.mkdir(outdir)
 
 os.chdir(inputdir)
 
-script = f"java -jar /opt/software/PGDSpider_2.1.1.3/PGDSpider2-cli.jar -inputfile {sys.argv[1]} -outputfile out/{sys.argv[2]}.phy -spid {path}/VCF_to_PHYLIP.spid"
+script = f"java -jar /opt/software/PGDSpider_2.1.1.3/PGDSpider2-cli.jar -inputfile {inputname} -outputfile out/{sys.argv[2]}.phy -spid {path}/VCF_to_PHYLIP.spid"
 subprocess.run(script, shell=True)
-print(outdir)
-print(inputdir)
 
-make_archive("out", "zip", "." , "out")
+make_archive("out", "zip", ".", "out")
