b827a0f12b5059f72686179a1e86d25610fca90c
liyu <liyu@microanaly.com>
2019-04-28 11:30:05 +0800

    :完善fasta序列长度小工具

--- app/static/program/fastalength/fastalength.py
+++ app/static/program/fastalength/fastalength.py
@@ -1,28 +1,28 @@
 import os,sys
 from shutil import make_archive
+from Bio import SeqIO
 
 infile=os.path.dirname(sys.argv[1])
 outfile= infile + "/out"
 length=int(sys.argv[2])
-fas = { }
-with open(sys.argv[1],'r') as f:
-    for line in f:
-        if line[0] == '>':
-            key = line.strip()
-            fas[key] = f.readline().strip()
 
-with open(outfile+'/outresult.fas','w') as f2:
-    for key in fas:
+
+with open(outfile+'/outresult.fas','w+') as f2:
+    for seq_record in SeqIO.parse(sys.argv[1], "fasta"):
+        fasta = seq_record
         start = 0
         end = start + length
-        f2.write(str(key)+'\n')
-        while True:
-            if end < len(fas[key]):
-                f2.write(str(fas[key][start:end]) + '\n')
-                start = end
-                end += length
-            elif end >= len(fas[key]):
-                f2.write(str(fas[key][start:]) + '\n')
-                break
+        f2.write(str(fasta.description)+'\n')
+        if length !=0:
+            while True:
+                if end < len(fasta.seq):
+                    f2.write(str(fasta.seq[start:end]) + '\n')
+                    start = end
+                    end += length
+                elif end >= len(fasta.seq):
+                    f2.write(str(fasta.seq[start:]) + '\n')
+                    break
+        else:
+            f2.write(str(fasta.seq[start:]) + '\n')
 
 make_archive(infile+ "/out", "zip",infile,"out")
